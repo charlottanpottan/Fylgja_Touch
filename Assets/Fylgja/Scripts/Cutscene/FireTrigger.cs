3a9ad3d630360ab38d05c11b6bc9b8d17e2ad4a8
Lena <lena@outhere.se>
2019-05-23 11:07:26 +0200

    Fire effects in shaman scene (not so nice looking yet).

--- Assets/Fylgja/Scripts/Cutscene/FireTrigger.cs
+++ Assets/Fylgja/Scripts/Cutscene/FireTrigger.cs
@@ -3,36 +3,42 @@ using System.Collections;
 
 public class FireTrigger : MonoBehaviour
 {
-	public GameObject[] objectsToActivate;
-	public AnimationClip animationToPlay;
+    public GameObject[] objectsToActivate;
+    public AnimationClip animationToPlay;
 
-	// Use this for initialization
-	void Start()
-	{
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.CompareTag("FireTrigger"))
-		{
-			ActivateObjects();
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("FireTrigger"))
+        {
+            ActivateObjects();
+        }
+    }
 
-	public void ActivateObjects()
-	{
-		foreach (GameObject go in objectsToActivate)
-		{
-			GetComponent<Animation>().Play(animationToPlay.name);
-			go.active = true;
-		}
-	}
+    public void ActivateObjects()
+    {
+        foreach (GameObject go in objectsToActivate)
+        {
+            GetComponent<Animation>().Play(animationToPlay.name);
+            if (!go)
+                Debug.Log("no go to activate on FireTrigger!!!!!!!!!!!!!!!!!!!!!!!!!!!............... " + name + " parent " + transform.parent.name);
+            else
+                go.active = true;
+        }
+    }
 
-	public void DeactivateObjects()
-	{
-		foreach (GameObject go in objectsToActivate)
-		{
-			go.active = false;
-		}
-	}
+    public void DeactivateObjects()
+    {
+        foreach (GameObject go in objectsToActivate)
+        {
+            if (!go)
+                Debug.Log("no go to deactivate on FireTrigger!!!!!!!!!!!!!!!!!!!!!!!!!!!............... " + name + " parent " + transform.parent.name);
+            else
+                go.active = false;
+        }
+    }
 }
