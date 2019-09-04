8ed0e220d2f6a4df9160c2f65f934e9407263ce4
Lena <lena@outhere.se>
2019-09-04 11:42:16 +0200

    Added version number so we can keep track of which version Charlotte is testing.

---
+++ Assets/Fylgja/Scripts/VersionDisplayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionDisplayer : MonoBehaviour
+{
+    [SerializeField] int version;
+
+    void OnGUI()
+    {
+        GUILayout.Space(30);
+        GUILayout.Label("Version " + version);
+    }
+}
