53184e2cb7f08c72dc649011428cd6f1b6a603de
Lena <lena@outhere.se>
2019-12-06 14:08:03 +0100

    Placing minimap at upper right corner (now working for all screen resolutions). Started using the new UI system but opened pandoras box with target star and fading. Not enough time to go start changing this.

---
+++ Assets/Fylgja/Scripts/MiniMapPlacer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapPlacer : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] MeshRenderer borderMeshRenderer;
+
+    void Update()
+    {
+        if (!cam)
+            return;
+
+        Bounds bounds = borderMeshRenderer.bounds;
+        Vector2 min = cam.WorldToScreenPoint(bounds.min);
+        Vector2 max = cam.WorldToScreenPoint(bounds.max);
+
+        float diff = max.y - min.y;
+
+        float z = transform.position.z;
+        Vector3 screenPosition = new Vector3(cam.pixelWidth - diff / 1.8f, cam.pixelHeight - diff / 1.8f, 0);
+        Vector3 wordPosition = cam.ScreenToWorldPoint(screenPosition);
+        wordPosition.z = z;
+        transform.position = wordPosition;
+    }
+}
