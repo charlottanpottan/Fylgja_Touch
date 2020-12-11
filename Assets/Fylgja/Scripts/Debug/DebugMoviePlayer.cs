04f2abad464c04e3574134634319e868af479084
Peter Bjorklund <piot@hotmail.com>
2020-12-11 12:36:54 +0100

    fix: start work on render movie to camera (#4)

---
+++ Assets/Fylgja/Scripts/Debug/DebugMoviePlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class DummyImplementation: ActorSceneComponentNotification
+{
+    public GameObject GetActor(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public SceneActor GetSceneActor(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IAvatar GetMainAvatar()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public AvatarToPlayerNotifications GetPlayerNotifications()
+    {
+        var notifications = new AvatarToPlayerNotifications();
+        notifications.player = new Player();
+        notifications.player.playerInteraction = new PlayerInteraction();
+
+        return notifications;
+    }
+
+    public FadeInFadeOut FadeInOut()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject GetGameObject()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsResuming()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class DebugMoviePlayer : MonoBehaviour
+{
+    public StartMovieQuestPart startMovie;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startMovie.ActInScene(new DummyImplementation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Fylgja/Scripts/Movie/MoviePlayerToCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class MoviePlayerToCamera : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Canvas blackBorderCanvas;
+    
+    PlayerInteraction playerInteraction;
+    bool movieIsStarted;
+    bool waitingForPlay;
+    void Start()
+    {
+        Debug.Log("Movie starts. Lets PlayerInteraction know that we are in a form of cutscene");
+        playerInteraction = GameObject.FindObjectOfType<PlayerInteraction>();
+        playerInteraction.OnCutsceneStart();
+    }
+
+    void Update()
+    {
+        videoPlayer.targetCamera = Camera.main;
+        blackBorderCanvas.worldCamera = Camera.main;
+        
+        if (waitingForPlay && videoPlayer.isPlaying)
+        {
+            waitingForPlay = false;
+            movieIsStarted = true;
+        }
+        
+        if (movieIsStarted && !videoPlayer.isPlaying)
+        {
+            movieIsStarted = false;
+            MoviePlayingDone();
+        }
+    }
+
+    private void MoviePlayingDone()
+    {
+        Debug.Log("Movie play stopped");
+    }
+}
